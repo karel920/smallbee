c67f52741c107ab7580443407ba21e8606a99b01
root <root@localhost.localdomain>
2021-02-17 22:25:53 +0800

    backup

--- public/js/app.js
+++ public/js/app.js
@@ -49897,9 +49897,9 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Volumes/Work/Web/smallbee/smallbee_admin/resources/js/app.js */"./resources/js/app.js");
-__webpack_require__(/*! /Volumes/Work/Web/smallbee/smallbee_admin/resources/sass/index.scss */"./resources/sass/index.scss");
-module.exports = __webpack_require__(/*! /Volumes/Work/Web/smallbee/smallbee_admin/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /var/www/html/smallbee/resources/js/app.js */"./resources/js/app.js");
+__webpack_require__(/*! /var/www/html/smallbee/resources/sass/index.scss */"./resources/sass/index.scss");
+module.exports = __webpack_require__(/*! /var/www/html/smallbee/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
